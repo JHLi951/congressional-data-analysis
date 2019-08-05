ed5589d5e6cff878701a80f27ecfd4102d7a001d
Jeffrey <jeffreyhli951@gmail.com>
2019-08-04 22:20:40 -0400

    updated gui to use a notebook

--- gui.py
+++ gui.py
@@ -1,51 +1,46 @@
 import tkinter as tk
 from tkinter import *
 import congressional_data
-
+from tkinter import ttk
 
 def get_member_info():
     name = "{} {}".format(e1.get(), e2.get())
     # print(name)
     info = congressional_data.get_specific_member(name)
-    linfo.config(text=info)
+    tinfo.delete('1.0', END)
+    tinfo.insert(INSERT, info)
+    tinfo.tag_add('center', '1.0', 'end')
 
-# def on_configure(event):
-#     # update scrollregion after starting 'mainloop'
-#     # when all widgets are in canvas
-#     canvas.configure(scrollregion=canvas.bbox('all'))
+root = tk.Tk()
+root.title("Congressional Data")
 
-m = tk.Tk()
-m.title("Congressional Data")
-
-# canvas = tk.Canvas(root)
-# canvas.pack(side=tk.LEFT)
-
-# scrollbar = tk.Scrollbar(root, command=canvas.yview)
-# scrollbar.pack(side=tk.LEFT, fill='y')
-# canvas.configure(yscrollcommand=scrollbar.set)
+nb = ttk.Notebook(root)
+f1 = Frame(root, width=500, height=500)
+f2 = Frame(root, width=500, height=500)
+nb.pack()
+nb.add(f1, text='Get Member Info')
+nb.add(f2, text='Get Member Spending')
 
 
-# canvas.bind('<Configure>', on_configure)
-
-# m = tk.Frame(canvas)
-# canvas.create_window((0, 0), window=m, anchor='nw')
-
-ltitle = Label(m, text='Congressional Data Book')
+ltitle = Label(f1, text='Congressional Data Book')
 ltitle.grid(row=0, column=1)
-lfname = Label(m, text='First Name')
+lfname = Label(f1, text='First Name')
 lfname.grid(row=1) 
-llname = Label(m, text='Last Name')
+llname = Label(f1, text='Last Name')
 llname.grid(row=2) 
-e1 = tk.Entry(m) 
-e2 = tk.Entry(m) 
-e1.grid(row=1, column=2) 
-e2.grid(row=2, column=2)
+e1 = tk.Entry(f1) 
+e2 = tk.Entry(f1) 
+e1.grid(row=1, column=1) 
+e2.grid(row=2, column=1)
 
-Button(master=m, text='Get Member Info', command=get_member_info).grid(row=3, column=1)
-linfo = Label(m, text='Member Info Here', wraplength=500)
-linfo.grid(row=4, column=1)
+get_info_button = Button(master=f1, text='Get Member Info', command=get_member_info)
+get_info_button.grid(row=3, column=1)
+# linfo = Label(f1, text='Member Info Here', wraplength=500)
+# linfo.grid(row=4, column=1)
+tinfo = Text(f1)
+tinfo.tag_configure('center', justify='center')
+tinfo.grid(row=4, column=1)
 
 
 
-
-m.mainloop()
+root.mainloop()
